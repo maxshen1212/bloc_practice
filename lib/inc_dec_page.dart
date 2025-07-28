5b904b1f5b663b0b23ffc93451e395663e4237c6
沈治翰 <maxshen1212@gmail.com>
2025-07-28 14:33:23 -0700

    0727 cubit test

---
+++ lib/inc_dec_page.dart
@@ -0,0 +1,35 @@
+import 'package:bloc_practice/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class IncDecPage extends StatelessWidget {
+  const IncDecPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterCubit = BlocProvider.of<CounterCubit>(context);
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: "Increace",
+            onPressed: () {
+              counterCubit.increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            heroTag: "Decreace",
+            onPressed: () {
+              counterCubit.decrement();
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ),
+    );
+  }
+}
